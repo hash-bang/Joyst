8638f718b8341a1726865ed03c0647866651ac08
Matt Carter <m@ttcarter.com>
2014-05-13 17:03:03 +1000

    Better handling of calls to JoystModel() without the model being a Joyst_Model

--- Joyst_Controller.php
+++ Joyst_Controller.php
@@ -179,6 +179,8 @@ class Joyst_Controller extends CI_Controller {
 		// Call function {{{
 		// Check the model is loaded and call the function
 		$this->load->model($model);
+		if (!is_subclass_of($this->$model, 'Joyst_Model'))
+			die("Use of \$this->JoystModel('$model') on a model that does not extend Joyst_Model");
 		//echo "Call \$this->$model->$func(" . json_encode($args) . ")<br/>";
 		$return = call_user_func_array(array($this->$model, $func), $args);
 		// }}}
